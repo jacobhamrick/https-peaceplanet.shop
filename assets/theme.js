c15baf6655ec0e2da76df000e8d1b1a19e114476
Jacob Hamrick <thejacobhamrick@gmail.com>
2020-07-08 21:53:37 -0400

    General build update. Wokring on menu.

--- assets/theme.js
+++ assets/theme.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function() {
+
   jQuery('.toggle-menu').on('click', function() {
     jQuery('.menu').toggleClass('openSesame');
     jQuery(this).toggleClass('active');
   });
 
-  jQuery('.toggle').on('click', function(){
-    jQuery('.menu-hide').removeClass('show');
-    jQuery('.menu-tab').removeClass('active');
+  jQuery('.dropdown-toggle').on('click', function() {
+    jQuery('.dropdown-nav').toggleClass('dropped');
+    jQuery('.dropdown-caret').toggleClass('rotated');
   });
 
   var $hamburger = jQuery(".hamburger");
